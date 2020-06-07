a405a9c0e30df92a6ae84d0f49567daa18d73176
Yurii <Yurri2002@gmail.com>
2020-06-07 19:46:56 +0300

    byte

--- lab_4.py
+++ lab_4.py
@@ -2,6 +2,9 @@ import sys
 
 
 image_in, image_out, scale = sys.argv[1], sys.argv[2], int(sys.argv[3])
+with open(image_in, 'rb') as f:
+    data = bytearray(f.read())
+print(data)
 print(image_in)
 print(image_out)
 print(scale)
